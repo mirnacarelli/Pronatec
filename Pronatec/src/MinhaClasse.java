a3e8bb119b686e17b3b072ea8e7af76e87299493
Mirna Maia <mirnacarelli@gmail.com>
2014-08-17 15:26:41 -0300

    Meu segundo commit com o eclipse.

--- Pronatec/src/MinhaClasse.java
+++ Pronatec/src/MinhaClasse.java
@@ -2,7 +2,7 @@
 public class MinhaClasse {
 
 	public static void main(String[] argv) {
-
-		System.out.println("Programa executado com sucesso");
+		System.out.println("Disciplina: Configuração de Ambiente");
+		System.out.println("Inserindo uma alteração de código no nosso projeto.");
 	}
 }
